6cfa126b1a10fc39f672074a48c15834ad830d4e
vinita <vinitamenaria2004@gmail.com>
2025-01-17 13:48:48 +0530

    updated

--- binarytodecimal.java
+++ binarytodecimal.java
@@ -1,23 +1,25 @@
+
 import java.util.Scanner;
 
 public class binarytodecimal {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your number");
         int num = sc.nextInt();
-       int result= bintodec(num);
-        System.out.println("Decimal value of "+num+" is :"+result);
+        int result = bintodec(num);
+        System.out.println("Decimal value of " + num + " is :" + result);
     }
 
     public static int bintodec(int binNum) {
         int pow = 0, dec = 0;
         while (binNum != 0) {
             int lastdigit = binNum % 10;
-           dec = dec + (lastdigit * (int) Math.pow(2, pow));
-        /* power function ko double  maanta h isliye typecast
+            dec = dec + (lastdigit * (int) Math.pow(2, pow));
+            /* power function ko double  maanta h isliye typecast
          krke int bna diya*/
             pow++;
-            binNum=binNum/10;
+            binNum = binNum / 10;
         }
         return dec;
     }
